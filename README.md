4a5b05f12b4868b242aaabf084edece1d30278d2
omasakun <masaya2002@outlook.com>
2018-11-18 20:52:42 +0900

    Rephrase "this script" -> "this library"

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ https://omasakun.github.io/import-htmls/demo/
 3. If you've found any issue, feel free to ask me to fix the issues (or, create a pull request).
 
 ## Description
-**Make sure that this script is loaded earlier than any script.**
+**Make sure that this library is loaded earlier than any script.**
 
-This script add an import tag.
+This library add an import tag.
 
-For example, `<import src="./button.html" />` will be replaced with the contents of ./button.html.  
+For example, `<import src="./button.html" />` will be replaced with the contents of `./button.html`.  
 
 By default, the import tag is replaced only when the page is read for the first time.  
 (Exception: the import tag written in the file imported by the import tag will be replaced)  
 If you want to interpret the import tag at other times, please call the importHTMLs function without arguments.
 
-The load event handler added between the time that this script is loaded and the window load event fires is called after importHTMLs is executed.
+The load event handler added between the time that this library is loaded and the window load event fires is called after importHTMLs is executed.
 
 The `<script src = "..." />` and `<script> /* some JS code */ </script>` written in the imported file is interpreted by the browser and the script to be loaded by that script tag will be loaded.
--- importHTMLs.js
+++ importHTMLs.js
@@ -22,17 +22,20 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 /**
-Make sure that this script is loaded earlier than any script.
+**Make sure that this library is loaded earlier than any script.**
 
-This script add an import tag.
-For example, <import src="./button.html" /> will be replaced with the contents of ./button.html.
-By the way, by default, the import tag is replaced only when the page is read for the first time.
-(Exception: the import tag written in the file imported by the import tag will be replaced)
+This library add an import tag.
+
+For example, `<import src="./button.html" />` will be replaced with the contents of `./button.html`.  
+
+By default, the import tag is replaced only when the page is read for the first time.  
+(Exception: the import tag written in the file imported by the import tag will be replaced)  
 If you want to interpret the import tag at other times, please call the importHTMLs function without arguments.
 
-The load event handler added between the time that this script is loaded and the window load event fires is called after importHTMLs is executed.
+The load event handler added between the time that this library is loaded and the window load event fires is called after importHTMLs is executed.
 
-The <script src = "..."> written in the imported file is interpreted by the browser and the script to be loaded by that script tag will be loaded.
+The `<script src = "..." />` and `<script>  some JS code  </script>` written in the imported file is interpreted by the browser and the script to be loaded by that script tag will be loaded.
+
 */
 (() => {
 	var listeners = [];
